fbfcd97d591965b47f39daaee2e7bad747f4ce68
JAVYMOON <javfrancs@gmail.com>
2020-08-05 22:21:40 -0400

    Eliminación de whatsapp prueba 1

---
+++ public/plugins/whatsapp/jquery-whatsapp.js
@@ -0,0 +1,6 @@
+<!--JQuery-->  
+<script type="text/javascript" src="jquery-3.3.1.min.js"></script>  
+<!--Floating WhatsApp css-->  
+<link rel="stylesheet" href="floating-wpp.min.css">  
+<!--Floating WhatsApp javascript-->  
+<script type="text/javascript" src="floating-wpp.min.js"></script>
